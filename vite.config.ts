2fb22b165754ef939107c6dc7bab07149b7486f1
nuVooo <sebastian.angelone@googlemail.com>
2026-06-01 18:47:41 +0200

    chore: scaffold vite + react + typescript project

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: "src/main.tsx",
+      formats: ["es"],
+      fileName: () => "ha-reactdash.js",
+    },
+    rollupOptions: {
+      output: { inlineDynamicImports: true },
+    },
+  },
+  test: {
+    globals: true,
+    environment: "node",
+  },
+});
